ed0deacac6fa7d7840994c66d7350118550558a8
slw8658 <slw8658@rit.edu>
2023-10-24 17:38:08 -0400

    cache me outside

--- PYMOCK/virtualenv/Scripts/activate.bat
+++ PYMOCK/virtualenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\mroja\RIT\SWEN-352\PYMOCK\virtualenv
+set VIRTUAL_ENV=C:\Users\swils\Fall2023\SWEN352\activity3\SWEN352-Group3\PYMOCK\virtualenv
 
 if not defined PROMPT set PROMPT=$P$G
 
